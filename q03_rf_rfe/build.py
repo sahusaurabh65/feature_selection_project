441c3794efe92b1d6f69e73ee026aaf89c0fd505
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-11-01 19:00:26 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,19 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+
+def rf_rfe(df):
+    X = df.drop('SalePrice',axis=1)
+    y = df['SalePrice']
+    
+    model = RandomForestClassifier()
+    rfe = RFE(model,n_features_to_select=len(X.columns)/2)
+    rfe = rfe.fit(X,y)
+    
+    return list(X.columns[rfe.support_])
+
+
+
+rf_rfe(data)
+
 
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -8,3 +9,19 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(data):
+    X = data.drop('SalePrice',axis=1)
+    y = data['SalePrice']
+    
+    model = RandomForestClassifier()
+    
+    sfm = SelectFromModel(model)
+    sfm.fit_transform(X,y)
+    
+    feature_name = list(X.columns[sfm.get_support()])
+    
+    return feature_name
+
+select_from_model(data)
+
+
